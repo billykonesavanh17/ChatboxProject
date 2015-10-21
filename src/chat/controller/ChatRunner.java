f549c9101432b9d50c2be9189a3818be4bbf3f0c
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-10-21 08:32:16 -0600

    Creating project and adding to repo.

---
+++ src/chat/controller/ChatRunner.java
@@ -0,0 +1,16 @@
+package chat.controller;
+
+/**
+ * Starts the Chatbot Program.
+ * @author Billy Konesavanh
+ * @version 1.0 10/21/15
+ */
+public class ChatRunner
+{
+	public static void main(String [] args)
+	{
+		ChatController myChatController = new ChatController();
+		myChatController.start();
+	}
+
+}
