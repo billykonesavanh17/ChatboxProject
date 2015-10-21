45f64ea464208ff1d8acf0bc98c85e290287d40f
Billy Konesavanh <billykonesavanh17@gmail.com>
2015-10-21 09:10:33 -0600

    Added ChatView and fixed error.

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -1,6 +1,19 @@
 package chat.controller;
 
+import chat.view.ChatView;
+
+/**
+ * Application controller for the Chatbot project.
+ * @author Billy Konesavanh
+ * @version 1.11 10/21/15 fixed error :)
+ */
 public class ChatController
 {
+	private ChatView myDisplay;
+	
+	public void start()
+	{
+		
+	}
 
 }
