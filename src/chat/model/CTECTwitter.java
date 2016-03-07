3766fbceb434c3612787f6a304a89909482fb465
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-03-07 09:15:31 -0700

    Made a method that can send a tweet

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -5,7 +5,7 @@ import twitter4j.*;
 
 /**
  * @author Billy Konesavanh
- * @version 0.2 Reference to Twitter4j
+ * @version 0.2 Made a method that can send a tweet
  * Interact with Twitter
  */
 
@@ -13,4 +13,17 @@ public class CTECTwitter
 {
 	private ArrayList<Status> statusList;
 	private ArrayList<String> wordList;
+	private Twitter chatbotTwitter;
+	
+	public CTECTwitter()
+	{
+		statusList = new ArrayList<Status>();
+		wordList = new ArrayList<String>();
+	}
+	
+	public void sendTweet(String tweet)
+	{
+		chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+	}
 }
+
