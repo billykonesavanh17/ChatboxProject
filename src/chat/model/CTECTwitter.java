80429b86f87842ddbc5da8ad93b03cc9c9e88b53
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-03-08 09:22:25 -0700

    Added properties to src and tweeted

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -33,7 +33,7 @@ public class CTECTwitter
 	{
 		try
 		{
-			chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen!");
+			chatbotTwitter.updateStatus("I just tweeted from my Java Chatbot program! #APCSRocks @CTECNow Thanks @cscheerleader & @codyhenrichsen! BillyBot");
 		}
 		catch (TwitterException error)
 		{
