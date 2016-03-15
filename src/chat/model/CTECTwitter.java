6d1ab345896e219802be4f40690bab42576e16d1
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-03-15 08:57:52 -0600

    Added importWordsToArray method

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -2,7 +2,10 @@ package chat.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import twitter4j.*;
+import java.io.*;
 import chat.controller.ChatController;
 
 /**
@@ -130,6 +133,38 @@ public class CTECTwitter
 				spot --;	//When you remove you have to have -- or you will skip over
 			}
 		}
+	}
+	
+	/*
+	 * Reads the commonWords.txt file and imports the supplied words to a String.
+	 */
+	private String [] importWordsToArray()
+	{
+		String [] boringWords;
+		int wordCount = 0;
+		try
+		{
+			Scanner wordFile = new Scanner(new File("commonWords.txt"));
+			while(wordFile.hasNext())
+			{
+				wordCount++;
+				wordFile.next();
+			}
+			wordFile.reset();
+			boringWords = new String[wordCount];
+			int boringWordCount = 0;
+			while(wordFile.hasNext())
+			{
+				boringWords[boringWordCount] = wordFile.next();
+				boringWordCount++;
+			}
+			wordFile.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			return new String[0];
+		}
+		return boringWords;
 	}
 }
 
